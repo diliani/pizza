cd06f3e8edbb1fcd65ff7a841c6fb6ad29fd85da
diliani <132509989+diliani@users.noreply.github.com>
2024-10-24 15:24:10 -0300

    sa

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -22,7 +22,7 @@ async function quantidadeUsuariosPorRede() {
         plot_bgcolor: getCSS('--bg-color'),
         paper_bgcolor: getCSS('--bg-color'),
         title: {
-            text: 'Redes sociais com mais usuários no mundo',
+            text: 'Os 10 Países Mais Populosos do Mundo em 2024',
             x: 0,
             font: {
                 color: getCSS('--primary-color'),
