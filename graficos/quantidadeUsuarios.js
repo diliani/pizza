664dfbd8f3c889e0ebb98d5533e3e17f0ded04c7
diliani <132509989+diliani@users.noreply.github.com>
2024-10-24 13:39:43 -0300

    kl

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -4,8 +4,8 @@ async function quantidadeUsuariosPorRede() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
-    const nomeDasRedes = Object.keys(dados)
-    const quantidadeDeUsuarios = Object.values(dados)
+    const nomeDasRedes = ['fffghgh','fgfhg','fghfghfgh']
+    const quantidadeDeUsuarios = ['','']
 
     const data = [
         {
