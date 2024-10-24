5a40bb03fa68e145ac1170a05ba167d87dd834bc
diliani <132509989+diliani@users.noreply.github.com>
2024-10-24 15:29:25 -0300

    ds

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -11,7 +11,7 @@ async function vizualizarInformacoesGlobais() {
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `A população mundial atingirá quase 8,2 bilhões de habitantes em meados de 2024, segundo as estimativas, um crescimento que deverá continuar na segunda metade deste século.`
+    paragrafo.innerHTML = `A população mundial atingirá quase 8,2 bilhões de habitantes em meados de 2024, segundo as estimativas, um crescimento que deverá continuar na segunda metade deste século. Estas informações foram tiradas do site do National Geographic Portugal.`
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
